c20f03a7772ed04c7d0ed23e7b59791e51c6d18d
miklesh <mikle.shupletsov@gmail.com>
2025-03-20 09:44:50 +0300

    Data added

---
+++ Data/LS_MS Data CDF/annotation_list.m
@@ -0,0 +1,5 @@
+annotation_list.m1 = ['ALA'; 'ASN'; 'PHE'; 'ARG'; 'CYS'];
+annotation_list.m2 = ['ASP'; 'GLN'; 'HIS'; 'TYR'; 'TRP'];
+annotation_list.m3 = ['GLY'; 'PRO'; 'ILE'; 'GLU'; 'LYS'];
+annotation_list.m4 = ['SER'; 'VAL'; 'THR'; 'LEU'; 'MET'];
+save annotation_list annotation_list;
